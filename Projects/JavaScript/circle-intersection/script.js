f99f10e7e41c7ccafb76e22536e7b37450faed44
Amdadul Islam <amdadullahrayhan@gmail.com>
2024-04-21 17:12:51 +0600

    added js project on finding if two circles intersect

--- Projects/JavaScript/circle-intersection/script.js
+++ Projects/JavaScript/circle-intersection/script.js
@@ -3,21 +3,18 @@ document.addEventListener("click", (e) => {
 
   let x = e.clientX;
   let y = e.clientY;
-
+  circleDetails.push({ x, y });
   // console.log("x-co-ordinates: ", e.clientX, "y-co-ordinate: ", e.clientY);
 
   const circle = document.createElement("div");
 
   circle.classList.add("circle");
-  circle.style.marginLeft = x - 50 + "px";
-  circle.style.marginTop = y - 50 + "px";
-  //   circle.style.width = "100px";
-  //   circle.style.height = "100px";
-  //   circle.style.borderRadius = "50%";
-  //   circle.style.border = "2px solid slateblue";
+  circle.style.marginLeft = x - 55 + "px";
+  circle.style.marginTop = y - 55 + "px";
   document.body.appendChild(circle);
-  circleDetails.push({ x: x, y: y });
+
   console.log(circleDetails);
+
   const circles = document.querySelectorAll(".circle");
 
   // console.log(circles);
